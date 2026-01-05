2851696ee394e611be2560c8fe79a958e0c35320
JIHAD____T69 <jihad1m6488223510@gmail.com>
2026-01-05 23:09:49 +0600

    Update handleReaction.js

--- includes/handle/handleReaction.js
+++ includes/handle/handleReaction.js
@@ -1,43 +1,46 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
     return function ({ event }) {
         const { handleReaction, commands } = global.client;
-        const { messageID, threadID } = event;
+        const { messageID, threadID, reaction, userID } = event; 
 
+        // 🔒 শুধু ADMINBOT ব্যবহার করতে পারবে
+        if (!global.config.ADMINBOT.includes(userID)) {
+            return; // non-admin হলে কিছুই হবে না
+        }
+
+        // ®️ নির্দিষ্ট reaction দিলে মেসেজ unsend
+        const unsendReactions = ['😾', '😡', '🥵'];
+        if (unsendReactions.includes(reaction)) {
+            return api.unsendMessage(messageID);
+        }
+
+        // অন্য reaction system
         if (handleReaction.length !== 0) {
-            const indexOfHandle = handleReaction.findIndex(e => e.messageID === messageID);
-
+            const indexOfHandle = handleReaction.findIndex(e => e.messageID == messageID);
             if (indexOfHandle < 0) return;
 
             const indexOfMessage = handleReaction[indexOfHandle];
             const handleNeedExec = commands.get(indexOfMessage.name);
 
-            if (!handleNeedExec) {
+            if (!handleNeedExec) 
                 return api.sendMessage(global.getText('handleReaction', 'missingValue'), threadID, messageID);
-            }
 
             try {
-                let getText2;
-
-                if (handleNeedExec.languages && typeof handleNeedExec.languages === 'object') {
+                // language system
+                var getText2;
+                if (handleNeedExec.languages && typeof handleNeedExec.languages == 'object') {
                     getText2 = (...value) => {
                         const react = handleNeedExec.languages || {};
-
-                        if (!react.hasOwnProperty(global.config.language)) {
+                        if (!react.hasOwnProperty(global.config.language))
                             return api.sendMessage(global.getText('handleCommand', 'notFoundLanguage', handleNeedExec.config.name), threadID, messageID);
-                        }
-
-                        let lang = handleNeedExec.languages[global.config.language][value[0]] || '';
-
-                        for (let i = value.length - 1; i >= 0; i--) {
-                            const expReg = new RegExp('%' + i, 'g');
+                        var lang = handleNeedExec.languages[global.config.language][value[0]] || '';
+                        for (var i = value.length; i > 0; i--) {
+                            const expReg = RegExp('%' + i, 'g');
                             lang = lang.replace(expReg, value[i]);
                         }
-
                         return lang;
                     };
-                } else {
-                    getText2 = () => {};
-                }
+                } else getText2 = () => {};
 
                 const Obj = {
                     api,
@@ -52,6 +55,7 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
 
                 handleNeedExec.handleReaction(Obj);
                 return;
+
             } catch (error) {
                 return api.sendMessage(global.getText('handleReaction', 'executeError', error), threadID, messageID);
             }
