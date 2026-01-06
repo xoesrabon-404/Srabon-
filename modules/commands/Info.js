7b55e5ebcdd72ef9afee161cb783f2f7aa55eb93
JIHAD____T69 <jihad1m6488223510@gmail.com>
2026-01-06 08:09:06 +0600

    Create Info.js

---
+++ modules/commands/Info.js
@@ -0,0 +1,84 @@
+module.exports.config = {
+ name: "info",
+ version: "1.2.6",
+ hasPermssion: 0,
+ credits: "Jihad",
+ description: "Bot information command",
+ commandCategory: "For users",
+ hide: true,
+ usages: "",
+ cooldowns: 5,
+};
+
+module.exports.run = async function ({ api, event, args, Users, Threads }) {
+ const { threadID } = event;
+ const request = global.nodemodule["request"];
+ const fs = global.nodemodule["fs-extra"];
+ const moment = require("moment-timezone");
+
+ const { configPath } = global.client;
+ delete require.cache[require.resolve(configPath)];
+ const config = require(configPath);
+
+ const { commands } = global.client;
+ const threadSetting = (await Threads.getData(String(threadID))).data || {};
+ const prefix = threadSetting.hasOwnProperty("PREFIX") ? threadSetting.PREFIX : config.PREFIX;
+
+ const uptime = process.uptime();
+ const hours = Math.floor(uptime / 3600);
+ const minutes = Math.floor((uptime % 3600) / 60);
+ const seconds = Math.floor(uptime % 60);
+
+ const totalUsers = global.data.allUserID.length;
+ const totalThreads = global.data.allThreadID.length;
+
+ const msg = `╭⭓ ⪩ 𝐁𝐎𝐓𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 ⪨
+│
+├─ 🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲 : ─꯭─⃝‌‌Jihad 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
+├─ ☢️ 𝗣𝗿𝗲𝗳𝗶𝘅 : ${config.PREFIX}
+├─ ♻️ 𝗣𝗿𝗲𝗳𝗶𝘅 𝗕𝗼𝘅 : ${prefix}
+├─ 🔶 𝗠𝗼𝗱𝘂𝗹𝗲𝘀 : ${commands.size}
+├─ 🔰 𝗣𝗶𝗻𝗴 : ${Date.now() - event.timestamp}ms
+│
+╰───────⭓
+
+╭⭓ ⪩ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ⪨
+│
+├─ 👑 𝗡𝗮𝗺𝗲 : Jihad
+├─ 📲 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+│ facebook.com/100086599998655
+├─ 💌 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
+│ m.me/100086599998655
+├─ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
+│ wa.me/+8801798907511
+│
+╰───────⭓
+
+╭⭓ ⪩ 𝗔𝗖𝗧𝗜𝗩𝗜𝗧𝗜𝗘𝗦 ⪨
+│
+├─ ⏳ 𝗔𝗰𝘁𝗶𝘃𝗲 𝗧𝗶𝗺𝗲 : ${hours}h ${minutes}m ${seconds}s
+├─ 📣 𝗚𝗿𝗼𝘂𝗽𝘀 : ${totalThreads}
+├─ 🧿 𝗧𝗼𝘁𝗮𝗹 𝗨𝘀𝗲𝗿𝘀 : ${totalUsers}
+╰───────⭓
+
+❤️ 𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝘂𝘀𝗶𝗻𝗴 🌺
+ 😍─꯭─⃝‌‌Jihae 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭😘`;
+
+ const imgLinks = [
+ "https://i.imgur.com/PgdBwHP.jpeg",
+ "https://i.imgur.com/qpZfe8A.jpeg",
+ "https://i.imgur.com/bA1917U.jpeg",
+ "https://i.imgur.com/PgdBwHP.jpeg",
+ ];
+
+ const imgLink = imgLinks[Math.floor(Math.random() * imgLinks.length)];
+
+ const callback = () => {
+ api.sendMessage({
+ body: msg,
+ attachment: fs.createReadStream(__dirname + "/cache/info.jpg")
+ }, threadID, () => fs.unlinkSync(__dirname + "/cache/info.jpg"));
+ };
+
+ return request(encodeURI(imgLink)).pipe(fs.createWriteStream(__dirname + "/cache/info.jpg")).on("close", callback);
+};
