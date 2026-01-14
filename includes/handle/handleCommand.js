594fe3a655030535ee3803dc87ad722c014ad47d
JIHAD____T69 <jihad1m6488223510@gmail.com>
2026-01-14 22:40:28 +0600

    Update handleCommand.js

--- includes/handle/handleCommand.js
+++ includes/handle/handleCommand.js
@@ -130,7 +130,7 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
 
     if (!ADMINBOT.includes(senderID)) {
       if (vipMode && !vipList.includes(senderID)) {
-        return api.sendMessage("⏤͟͟͞͞⭕☻\n⏤͟͟͞͞𝑂𝑁𝐿𝑌 𝑉𝐼𝑃 𝑈𝑆𝐸𝑅𝑆 𝐶𝐴𝑁 𝑈𝑆𝐸 𝑇𝐻𝐼𝑆 𝐶𝑂𝑀𝑀𝐸𝑁𝐷", threadID, messageID);
+        return api.sendMessage("⏤͟͟͞͞⭕☻\n⏤͟͟͞͞𝑂𝑁𝐿𝑌 ⏤͟͟͞͞𝐴𝐷𝑀𝐼𝑁 ⏤͟͟͞͞𝐶𝐴𝑁 ⏤͟͟͞͞𝑈𝑆𝐸 ⏤͟͟͞͞𝑇𝐻𝐼𝑆 ⏤͟͟͞͞𝐶𝑂𝑀𝑀𝐸𝑁𝐷 ᥫ᭡", threadID, messageID);
       }
     }
 
