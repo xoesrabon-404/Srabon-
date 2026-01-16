a0be7ad07edbf96f892bd6192ff98a011a8610d7
JIHAD____T69 <jihad1m6488223510@gmail.com>
2026-01-17 01:51:29 +0600

    Update autodl.js

--- modules/commands/autodl.js
+++ modules/commands/autodl.js
@@ -32,12 +32,7 @@ module.exports.handleEvent = async function ({ api, event }) {
 
     // Send detect message
     const detectBox =
-`╔══════════════════╗
-║ ⏤͟͟͞͞𝑃⃠𝐿𝐴𝑇𝐹𝑂𝑅𝑀 𝐷𝐸𝐹𝐸𝐶𝑇𝐸𝐷
-╠══════════════════╣
-║ 📍 𝑃𝑙𝑎𝑡𝑓𝑜𝑟𝑚𝑒𝑟 : ${site}
-║ ⬇️ 𝐷𝑜𝑤𝑛𝑙𝑜𝑎𝑑𝑖𝑛𝑔.......
-╚══════════════════╝`;
+``;
 
     // প্রথম মেসেজ পাঠানো এবং ⬇️ রিয়েক্ট
     api.sendMessage(detectBox, event.threadID, async (err, info) => {
@@ -62,10 +57,12 @@ module.exports.handleEvent = async function ({ api, event }) {
         // Send downloaded file
         const doneBox =
 `╔════════════════════╗
-║⏤͟͟͞͞⃠𝐷𝑂𝑊𝑁𝐿𝑂𝐴𝐷 ⏤͟͟͞͞𝐶𝑂𝑀𝑃𝐿𝐸𝑇𝐸𝐿𝑌 
+║⏤͟͟͞͞𝐷𝑂𝑊𝑁𝐿𝑂𝐴𝐷 ⏤͟͟͞͞𝐶𝑂𝑀𝑃𝐿𝐸𝑇𝐸𝐿𝑌 
 ╠════════════════════╣
-║ 🅹🅸🅷🅰🅳 💮
+║ ⏤͟͟͞͞🅹🅸🅷🅰🅳 💮🎀 
+║
 ║ 📍 𝑃𝑙𝑎𝑡𝑓𝑜𝑟𝑚 : ${site}
+║
 ║ 🎬 𝑇𝑖𝑡𝑙𝑒 : ${title}
 ╚════════════════════╝`;
 
