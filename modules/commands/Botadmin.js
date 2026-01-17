98ab0e387148cd43c4cd51826b6ee09d57998e99
JIHAD____T69 <jihad1m6488223510@gmail.com>
2026-01-17 12:24:49 +0600

    Create Botadmin.js

---
+++ modules/commands/Botadmin.js
@@ -0,0 +1,67 @@
+const axios = require("axios");
+const request = require("request");
+const fs = require("fs-extra");
+const moment = require("moment-timezone");
+
+module.exports.config = {
+ name: "Jihad",
+ version: "1.0.0",
+ hasPermssion: 0,
+ credits: "JIHAD ",
+ description: "Show Owner Info",
+ commandCategory: "info",
+ usages: "Bot Admin",
+ cooldowns: 2
+};
+
+module.exports.run = async function({ api, event }) {
+ const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+
+ const callback = () => api.sendMessage({
+ body: `
+┌───────────────ᜊ        
+│⏤͟͟͞͞𝐴⃠𝐷𝑀𝐼𝑁 ⏤͟͟͞͞𝐷𝑇𝐸𝐴𝐿𝐼𝑆                                
+├───────────────ᥫ᭡
+│    𝐴𝑠𝑠𝑎𝑙𝑎𝑚𝑢 𝐴𝑙𝑎𝑖𝑘𝑢𝑚 
+│
+│ 💝 𝑁𝑎𝑚𝑒        : ⏤͟͟͞͞𝐽𝐼 𝐻𝐴𝐷 𝐻𝐴𝑆𝐴𝑁 
+│ 🌸 𝐺𝑒𝑛𝑑𝑒𝑟        : 𝑀𝑎𝑙𝑒
+│ 💓 𝑅𝑒𝑎𝑙𝑎𝑡𝑖𝑜𝑛       : 𝑆𝑖𝑛𝑔𝑙𝑒 𝑃𝑟𝑜 𝑀𝑎𝑥
+│ 💋 𝐴𝑔𝑒          : 𝟏𝟖+
+│ ❤️‍🔥 𝑅𝑒𝑙𝑖𝑔𝑖𝑜𝑛     : 𝐼𝑠𝑙𝑎𝑚
+│ 🩷 𝐸𝑑𝑢𝑐𝑎𝑡𝑖𝑜𝑛      : 𝑆𝑆𝐶 (𝟐𝟎𝟐5)
+│ 💜 𝐴𝑑𝑑𝑟𝑒𝑠𝑠      : 𝑀𝑖𝑟𝑝𝑢𝑟 1 
+│ 🖤 𝐹𝑎𝑣𝑜𝑟𝑖𝑡𝑒 𝐶𝑜𝑙𝑜𝑟𝑠 : 𝑏𝑙𝑎𝑐𝑘  
+│ ♥️ 𝐻𝑒𝑖𝑔ℎ𝑡       : 5.9
+│ 💛 𝑆ℎ𝑜𝑘ℎ        : 𝐹𝑟𝑒𝑒 𝑓𝑖𝑟𝑒
+│ ☠️ 𝑊𝑜𝑟𝑘         : 𝐻𝑎𝑐𝑘𝑖𝑛𝑔
+│ 👀 𝐸𝑚𝑜𝑡𝑖𝑜𝑛𝑎  : 𝐻𝑎𝑐𝑘𝑖𝑛𝑔 𝑎𝑛𝑑 𝐵𝑙𝑎𝑐𝑘 𝑀𝑎𝑔𝑖𝑐 
+│    
+│ 𝑀𝐸𝑆𝑆𝐴𝐺𝐸 𝐶𝐻𝐴𝑇 𝐵𝑂𝑇 𝐴𝐷𝑀𝐼𝑁 ⏤͟͟͞͞𝐽𝐼𝐻𝐴𝐷 𝐻𝐴𝑆𝐴𝑁 
+│
+└───────────────ᥫ᭡          
+
+┌───────────────ᥫ᭡
+│ ⏤͟͟͞͞𝐶𝑂𝑁𝑇𝐴𝐶𝑇 ⏤͟͟͞͞𝐿𝐼𝑁𝐾
+├───────────────ᜊ
+│ 📘 ⏤͟͟͞͞𝐹𝑎𝑐𝑒𝑏𝑜𝑜𝑘
+│ facebook.com/100086599998655
+│ 💬 ⏤͟͟͞͞𝑊ℎ𝑎𝑡'𝑠 𝐴𝑝𝑝
+│ wa.me/01798907511
+│ 📱 ⏤͟͟͞͞𝑇𝑖𝑘𝑇𝑜𝑘 
+│ jihad__hasan__64
+└───────────────ᥫ᭡
+
+┌───────────────ᥫ᭡
+│ 🕒 𝗨𝗽𝗱𝗮𝘁𝗲𝗱 𝗧𝗶𝗺𝗲
+├───────────────ᜊ
+│ ${time}
+└───────────────ᥫ᭡
+ `,
+ attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+ }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
+
+ return request("https://i.imgur.com/0i7Z624.jpeg")
+ .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
+ .on('close', () => callback());
+};
