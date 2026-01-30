d726371a18249ce303fc6bc3ddfd1eb8c7b79de9
JIHAD____T69 <jihad1m6488223510@gmail.com>
2026-01-30 18:32:52 +0600

    Update pp.js

--- modules/commands/pp.js
+++ modules/commands/pp.js
@@ -15,7 +15,7 @@ const fs = global.nodemodule["fs-extra"];
  if(event.type == "message_reply") { 
  let name = await Users.getNameUser(event.messageReply.senderID) 
 	uid = event.messageReply.senderID
-	var callback = () => api.sendMessage({body:`༺ᰔ⏤͟͟͞͞𝐽⃠𝑖𝐻𝐴𝐷 ⏤͟͟͞͞𝐻⃝𝐴𝑆𝐴𝑁ツ༻`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+	var callback = () => api.sendMessage({body:`༺☻ 𝐽𝑖ℎ𝑎𝑑 𝐻𝑎𝑠𝑎𝑛ツ༻`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
  () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
  return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
  () => callback()); 
