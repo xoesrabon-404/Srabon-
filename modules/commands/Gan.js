7eba64c93211772317a9d522e72281ae8b14fcae
JIHAD____T69 <jihad1m6488223510@gmail.com>
2026-03-01 22:54:21 +0600

    Update Gan.js

--- modules/commands/Gan.js
+++ modules/commands/Gan.js
@@ -13,9 +13,8 @@ module.exports.config = {
   cooldowns: 0
 };
 
-// 🎧 84 ta sad song link (first 42 mp3, next 42 mp4 converted to mp3, TikTok লিংক বাদ)
+// 🎧 42 ta sad song link (audio only)
 const deepSongs = [
-  // প্রথম 42 mp3 লিংক
   "https://files.catbox.moe/npzr5e.mp3",
   "https://files.catbox.moe/5ydg4o.mp3",
   "https://files.catbox.moe/plvu6e.mp3",
@@ -34,7 +33,7 @@ const deepSongs = [
   "https://files.catbox.moe/wvbd4s.mp3",
   "https://files.catbox.moe/3bvlso.mp3",
   "https://files.catbox.moe/pydnnw.mp3",
-  "https://files.catbox.moe/cxhmq2.mp3",
+  "https://files.catbox.moe/cxhmq2.mp4",
   "https://files.catbox.moe/i2n8y4.mp3",
   "https://files.catbox.moe/07cnwl.mp3",
   "https://files.catbox.moe/6tx7g5.mp3",
@@ -42,6 +41,7 @@ const deepSongs = [
   "https://files.catbox.moe/e07g3g.mp3",
   "https://files.catbox.moe/ihcw9h.mp3",
   "https://files.catbox.moe/1qy9o8.mp3",
+  "https://files.catbox.moe/yepi69.mp4",
   "https://files.catbox.moe/q5f21o.mp3",
   "https://files.catbox.moe/0yqdwg.mp3",
   "https://files.catbox.moe/36jnhy.mp3",
@@ -49,53 +49,14 @@ const deepSongs = [
   "https://files.catbox.moe/4r9nt8.mp3",
   "https://files.catbox.moe/qk7f3w.mp3",
   "https://files.catbox.moe/m7hy14.mp3",
+  "https://files.catbox.moe/bln6en.mp4",
+  "https://files.catbox.moe/rpl5uy.mp4",
+  "https://files.catbox.moe/1fg3k4.mp4",
   "https://files.catbox.moe/xftwwi.mp3",
   "https://files.catbox.moe/zrkary.mp3",
   "https://files.catbox.moe/f98rp3.mp3",
   "https://files.catbox.moe/sm4h5z.mp3",
-
-  // নতুন 42 লিংক mp3 হিসেবে
-  "https://files.catbox.moe/igy1oq.mp3",
-  "https://files.catbox.moe/f3pyt2.mp3",
-  "https://files.catbox.moe/1a2vn5.mp3",
-  "https://files.catbox.moe/9zyc9z.mp3",
-  "https://files.catbox.moe/ksk5iz.mp3",
-  "https://files.catbox.moe/u6o7g3.mp3",
-  "https://files.catbox.moe/9vtcgv.mp3",
-  "https://files.catbox.moe/cbxvh8.mp3",
-  "https://files.catbox.moe/o65uvg.mp3",
-  "https://files.catbox.moe/wluj21.mp3",
-  "https://files.catbox.moe/gqg0gh.mp3",
-  "https://files.catbox.moe/8gsu80.mp3",
-  "https://files.catbox.moe/9zzm2h.mp3",
-  "https://files.catbox.moe/n4u0sl.mp3",
-  "https://files.catbox.moe/dv99cu.mp3",
-  "https://files.catbox.moe/wt0h8a.mp3",
-  "https://files.catbox.moe/6kcjtb.mp3",
-  "https://files.catbox.moe/kn8dt2.mp3",
-  "https://files.catbox.moe/jood8u.mp3",
-  "https://files.catbox.moe/5u4y9r.mp3",
-  "https://files.catbox.moe/j3yb4a.mp3",
-  "https://files.catbox.moe/jcnlmm.mp3",
-  "https://files.catbox.moe/x15zbm.mp3",
-  "https://files.catbox.moe/5jxa41.mp3",
-  "https://files.catbox.moe/0xi82q.mp3",
-  "https://files.catbox.moe/5nfppj.mp3",
-  "https://files.catbox.moe/ipidzd.mp3",
-  "https://files.catbox.moe/0jgagi.mp3",
-  "https://files.catbox.moe/zb8zxp.mp3",
-  "https://files.catbox.moe/913by6.mp3",
-  "https://files.catbox.moe/cm1in4.mp3",
-  "https://files.catbox.moe/aeg9ca.mp3",
-  "https://files.catbox.moe/pl0xox.mp3",
-  "https://files.catbox.moe/yh9qzi.mp3",
-  "https://files.catbox.moe/j4wet1.mp3",
-  "https://files.catbox.moe/czmoe7.mp3",
-  "https://files.catbox.moe/a801mq.mp3",
-  "https://files.catbox.moe/z71fwi.mp3",
-  "https://files.catbox.moe/j235wd.mp3",
-  "https://files.catbox.moe/nduel7.mp3",
-  "https://files.catbox.moe/n7by70.mp3"
+  "https://files.catbox.moe/65miuh.mp4"
 ];
 
 const songProgress = {};
@@ -143,7 +104,7 @@ async function sendSong(api, threadID, index, replyToID) {
 
     api.sendMessage(
       {
-        body: "⏤͟͟͞͞𝐽𝐼𝐻𝐴𝐷☻ ⏤͟͟͞͞𝐻𝐴𝑆𝐴𝑁☻",
+        body: "ᥫ᭡ 🅹🅸🅷️🅰🅳 🅷️🅰🆂️🅰🅽 ࿐",
         attachment: fs.createReadStream(filePath)
       },
       threadID,
