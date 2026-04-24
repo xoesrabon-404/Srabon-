aa97c68e78293344540094d014c24568f2b1b1ec
xoesrabon-404 <borshaakther2008@gmail.com>
2026-04-25 02:41:29 +0400

    Update and rename upTime.js to uptime.js

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -31,7 +31,7 @@ module.exports.run = async function ({ api, event }) {
     const minutes = Math.floor((uptime % 3600) / 60);
     const seconds = Math.floor(uptime % 60);
     const ping = Date.now() - event.timestamp;
-    const owner = "🔰SRABON🔰";
+    const owner = "🔰srabon🔰";
 
     // ✍️ Base text style
     ctx.fillStyle = "#FFFFFF";
